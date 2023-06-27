be6962d21f8f7ab3bff02565e807533d3ad47d97
JM Hands <69059890+jmhands@users.noreply.github.com>
2023-06-27 09:32:34 -0700

    Update format.sh
    instead of skipping boot drive just skip every drive that is mounted

--- mount/format.sh
+++ mount/format.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-boot_drive_raw=$(df -h / | tail -1 | awk '{gsub("/dev/", "", $1); print $1}' | sed 's/[0-9]//g')
-
-# List all unmounted drives excluding the boot drive and its partitions and nvme
-drives=$(lsblk -rno name,mountpoint | awk -v boot_drive_raw="$boot_drive_raw" '$1 ~ "^sd" && $1 != boot_drive_raw && $1 !~ boot_drive_raw"[0-9]+$" && $2==""{print $1}')
+# Find all drives that are not mounted
+drives=$(lsblk -nlo NAME,MOUNTPOINT | awk '! /\// {print $1}' | grep -E '^sd[b-z]$|^sd[b-z][a-z]$|^sd[b-z][a-z][0-9]$')
 
 # Prompt user for confirmation
 echo "The following drives will be formatted:"
-echo "$drives"
+for drive in $drives; do
+  echo "/dev/$drive"
+done
 read -p "Do you want to proceed with formatting (y/n)? " confirm
 
 if [[ $confirm =~ ^[Yy]$ ]]
@@ -15,7 +15,7 @@ then
   # Format drives asynchronously
   for drive in $drives
   do
-    echo "Formatting drive $drive"
+    echo "Formatting drive /dev/$drive"
     sudo mkfs.ext4 -m 0 -T largefile4 -F /dev/$drive &
   done
 else
